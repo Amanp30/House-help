a1b4c35179b77bcd7899807c66710ad76e49091b
Aman Pareek <amanpareek300@gmail.com>
2024-04-30 11:49:08 +0530

    change to MONGO_URI

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 const CONFIG = {
   port: process.env.PORT || 9000,
   mongo: {
-    uri: process.env.MONGO_URL || "mongodb://127.0.0.1:27017",
+    uri: process.env.MONGO_URI || "mongodb://127.0.0.1:27017",
   },
   jwtSecret: process.env.JWT_SECRET || "jkl!±@£!@ghj1237",
 };

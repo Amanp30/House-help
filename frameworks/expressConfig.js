cda659bbc64077f254a65327118e35e56737a731
Aman Pareek <amanpareek300@gmail.com>
2024-04-30 11:34:44 +0530

    enable helmet

--- frameworks/expressConfig.js
+++ frameworks/expressConfig.js
@@ -3,7 +3,7 @@ const { default: helmet } = require("helmet");
 const morgan = require("morgan");
 
 function expressConfig(app, express) {
-  // app.use(helmet());
+  app.use(helmet());
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
